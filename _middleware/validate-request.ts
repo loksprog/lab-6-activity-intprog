efa074fb85e0c3e0610487b0d564dde2d1fb38b1
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2026-04-21 13:16:49 +0800

    feat: add error-handler and validate-request middlwares

---
+++ _middleware/validate-request.ts
@@ -0,0 +1,18 @@
+import { abort } from "node:process";
+
+export default validateRequest;
+
+function validateRequest(req: any, next: any, schema: any) {
+  const options = {
+    abortEarly: false, //include all errors
+    allowUnknown: true, // ignore unknown erros
+    stripUnknown: true, // remove unknown erros
+  };
+  const { error, value } = schema.valdiate(req.body, options);
+  if (error) {
+    next(`Valdiation error: ${error.details.map((x) => x.message).join(", ")}`);
+  } else {
+    req.body = value;
+    next();
+  }
+}
