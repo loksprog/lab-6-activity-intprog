b1848d3d45d1e48e86d29b797df592805e0d4dc6
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2026-04-28 09:01:03 +0800

    docs: add swagger documentation and helper

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-const swaggerDocument = YAML.load('./swagger.yaml');
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
